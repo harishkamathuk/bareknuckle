5b61e1e857fd7fc14207b45c1274caf03f38621a
Harish Kamath <bareknuckle@harishkamath.com>
2016-09-06 12:12:11 +0000

    Working Blog application from ZF Turorial

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -18,7 +18,9 @@ return array(
      
     'controllers' => array(
         'factories' => array(
-            'Blog\Controller\List' => 'Blog\Factory\ListControllerFactory'
+            'Blog\Controller\List' => 'Blog\Factory\ListControllerFactory',
+            'Blog\Controller\Write' => 'Blog\Factory\WriteControllerFactory',
+            'Blog\Controller\Delete' => 'Blog\Factory\DeleteControllerFactory'            
         )
     ),    
 
@@ -26,7 +28,7 @@ return array(
         // Open configuration for all possible routes
         'routes' => array(
             // Define a new route called "post"
-            'post' => array(
+            'blog' => array(
                 // Define the routes type to be "Zend\Mvc\Router\Http\Literal", which is basically just a string
                 'type' => 'literal',
                 // Configure the route itself
@@ -37,7 +39,58 @@ return array(
                     'defaults' => array(
                         'controller' => 'Blog\Controller\List',
                         'action'     => 'index',
-                    )
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes'  => array(
+                    'detail' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route'    => '/:id',
+                            'defaults' => array(
+                                'action' => 'detail'
+                            ),
+                            'constraints' => array(
+                               'id' => '[1-9]\d*'
+                           )
+                        )
+                    ),
+                    'add' => array(
+                        'type' => 'literal',
+                        'options' => array(
+                            'route'    => '/add',
+                            'defaults' => array(
+                                'controller' => 'Blog\Controller\Write',
+                                'action'     => 'add'
+                            )
+                        )
+                    ),
+                    'edit' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route'    => '/edit/:id',
+                            'defaults' => array(
+                                'controller' => 'Blog\Controller\Write',
+                                'action'     => 'edit'
+                            ),
+                            'constraints' => array(
+                                'id' => '\d+'
+                            )
+                        )
+                    ),       
+                    'delete' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route'    => '/delete/:id',
+                            'defaults' => array(
+                                'controller' => 'Blog\Controller\Delete',
+                                'action'     => 'delete'
+                            ),
+                            'constraints' => array(
+                                'id' => '\d+'
+                            )
+                        )
+                    ),                                                 
                 )
             )
         )
--- module/Blog/src/Blog/Factory/ZendDbSqlMapperFactory.php
+++ module/Blog/src/Blog/Factory/ZendDbSqlMapperFactory.php
@@ -5,6 +5,8 @@
  use Blog\Mapper\ZendDbSqlMapper;
  use Zend\ServiceManager\FactoryInterface;
  use Zend\ServiceManager\ServiceLocatorInterface;
+ use Zend\Stdlib\Hydrator\ClassMethods;
+ use Blog\Model\Post;
 
  class ZendDbSqlMapperFactory implements FactoryInterface
  {
@@ -17,7 +19,9 @@
      public function createService(ServiceLocatorInterface $serviceLocator)
      {
          return new ZendDbSqlMapper(
-             $serviceLocator->get('Blog\Mapper\ZendDbSqlMapper')
+             $serviceLocator->get('Zend\Db\Adapter\Adapter'),
+             new ClassMethods(false),
+             new Post()
          );
      }
  }
