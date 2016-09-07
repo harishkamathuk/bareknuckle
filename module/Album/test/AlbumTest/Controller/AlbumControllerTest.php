75fc51db15cb64122452e54744067f49110b971b
Harish Kamath <bareknuckle@harishkamath.com>
2016-09-07 12:41:47 +0000

    Updating the test cases for Album from ZF tutorial

---
+++ module/Album/test/AlbumTest/Controller/AlbumControllerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AlbumTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class AlbumControllerTest extends AbstractHttpControllerTestCase
+{
+
+	// protected $traceError = true;
+
+    public function setUp()
+    {
+        $this->setApplicationConfig(
+            // include '/home/bitnami/htdocs/bareknuckle/config/application.config.php'
+            include 'config' . DIRECTORY_SEPARATOR . 'application.config.php'
+        );
+        parent::setUp();
+    }
+
+	public function testIndexActionCanBeAccessed()
+	{
+	    $albumTableMock = $this->getMockBuilder('Album\Model\AlbumTable')
+	                            ->disableOriginalConstructor()
+	                            ->getMock();
+
+	    /* $albumTableMock->expects($this->once())
+	                    ->method('fetchAll')
+	                    ->will($this->returnValue(array())); */
+
+	    $serviceManager = $this->getApplicationServiceLocator();
+	    $serviceManager->setAllowOverride(true);
+	    $serviceManager->setService('Album\Model\AlbumTable', $albumTableMock);
+
+	    $this->dispatch('/album');
+	    // $this->assertResponseStatusCode(200);
+
+	    $this->assertModuleName('Album');
+	    $this->assertControllerName('Album\Controller\Album');
+	    $this->assertControllerClass('AlbumController');
+	    $this->assertMatchedRouteName('album');
+    }  
+}
+
+
+?>
